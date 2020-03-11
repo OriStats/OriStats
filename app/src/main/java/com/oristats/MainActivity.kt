0352d6576ae48d6947dcc788959e51a87ff41d35
joaolopes <joaolopes1999@gmail.com>
2020-03-11 22:00:05 +0000

    já com clock

--- app/src/main/java/com/oristats/MainActivity.kt
+++ app/src/main/java/com/oristats/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
 
-        //##############################
+      /*  //##############################
         //Stopwatch implementation code
         //#############################
         val meter = findViewById<Chronometer>(R.id.c_meter)//gets chronometer from the XML using the proper id
@@ -55,7 +55,7 @@ class MainActivity : AppCompatActivity() {
                         R.string.stopped),
                     Toast.LENGTH_SHORT).show()
             }
-        })
+        })*/
 
 
         //This part of the code refers to the button
--- app/src/main/java/com/oristats/ui/main/Stopwatch.kt
+++ app/src/main/java/com/oristats/ui/main/Stopwatch.kt
@@ -1,12 +1,14 @@
 package com.oristats.ui.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.oristats.R
+import kotlinx.android.synthetic.main.stopwatch_fragment.*
+
 
 class Stopwatch : Fragment() {
 
@@ -14,10 +16,40 @@ class Stopwatch : Fragment() {
         fun newInstance() = Stopwatch()
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.stopwatch_fragment, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //val meter =R.id.c_meter//gets chronometer from the XML using the proper id
+        //val btn =R.id.btStart//gets the button from the XML
+
+        btStart?.setOnClickListener(
+            object : View.OnClickListener {
+
+                var isWorking = false
+
+                override fun onClick(v: View) {
+                    if (!isWorking) {
+                        c_meter.start()
+                        //print("entrou")
+                        isWorking = true
+                    } else {
+                        c_meter.stop()
+                        isWorking = false
+                    }
+
+                    btStart.setText(if (isWorking) R.string.start else R.string.stop)
+                    //Toast.makeText(this@Stopwatch, getString(if (isWorking) R.string.working else R.string.stopped), Toast.LENGTH_SHORT).show()
+
+                }
+            })
     }
 }
