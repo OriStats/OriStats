64dca8a220934e295bc7d6071fd5d23e489aaa6d
joaolopes <joaolopes1999@gmail.com>
2020-03-11 01:24:54 +0000

    first attempt mesquita

--- app/src/main/java/com/oristats/MainActivity.kt
+++ app/src/main/java/com/oristats/MainActivity.kt
@@ -8,7 +8,12 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import com.oristats.ui.main.SectionsPagerAdapter
+import android.widget.Chronometer//chronometer imports
+import android.view.View
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +25,38 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
+
+        //##############################
+        //Stopwatch implementation code
+        //#############################
+        val meter = findViewById<Chronometer>(R.id.c_meter)//gets chronometer from the XML using the proper id
+        val btn = findViewById<Button>(R.id.btStart)//gets the button from the XML
+
+        btn?.setOnClickListener(object : View.OnClickListener {
+
+            var isWorking = false
+
+            override fun onClick(v: View) {
+                if (!isWorking) {
+                    meter.start()
+                    //print("entrou")
+                    isWorking = true
+                } else {
+                    meter.stop()
+                    isWorking = false
+                }
+
+                btn.setText(if (isWorking) R.string.start else R.string.stop)
+
+                Toast.makeText(this@MainActivity, getString(
+                    if (isWorking)
+                        R.string.working
+                    else
+                        R.string.stopped),
+                    Toast.LENGTH_SHORT).show()
+            }
+        })
+
 
         //This part of the code refers to the button
         val fab: FloatingActionButton = findViewById(R.id.fab)
