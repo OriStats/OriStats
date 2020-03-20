8543786c05ffc4aeefa43d0b5e97ddb683a7cb35
heitor-osterdahl <heitor.osterdahl@gmail.com>
2020-03-20 13:28:24 +0000

    three_dots_menu added.

--- app/src/main/java/com/oristats/MainActivity.kt
+++ app/src/main/java/com/oristats/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.oristats
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
@@ -29,8 +33,41 @@ class MainActivity : AppCompatActivity() {
          */
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.three_dots_menu, menu)
+        return true
+    }
 
-
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.Settings -> {
+                Toast.makeText(this, "Settings Selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.Export -> {
+                Toast.makeText(this, "Export Data Selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.Import -> {
+                Toast.makeText(this, "Import Data Selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.Help -> {
+                Toast.makeText(this, "Help Selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.About -> {
+                Toast.makeText(this, "About Selected", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.Donate -> {
+                Toast.makeText(this, "Donate Selected", Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
 }
